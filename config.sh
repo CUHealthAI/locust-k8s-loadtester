2491b1510a151cf72810fc1e0ed08b16dbfd508d
faisal <falquaddoomi@gmail.com>
2021-09-14 16:16:22 +0000

    Updated default host, logfile for locust image

--- config.sh
+++ config.sh
@@ -4,6 +4,6 @@ REGION=us-central1
 ZONE=${REGION}-a
 PROJECT="monarch-stack-loadtest"
 CLUSTER=monarch-load-test
-TARGET="monarch-test.ddns.net"
+TARGET="api.monarch-test.ddns.net"
 SCOPE="https://www.googleapis.com/auth/cloud-platform"
 CUR_GIT_HASH=git-$( git rev-parse --short HEAD )
